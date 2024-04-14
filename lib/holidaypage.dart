1b233491802f6423b7dfa32022ef5b262a5c7375
Connor <resistingprawn5@gmail.com>
2024-04-14 00:50:16 -0600

    added holiday page; doesn't show holidays yet

---
+++ lib/holidaypage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HolidayPage extends StatefulWidget {
+  @override
+  _HolidayPageState createState() => _HolidayPageState();
+}
+
+class _HolidayPageState extends State<HolidayPage> {
+  List<dynamic> _holidays = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchHolidays();
+  }
+
+  Future<void> fetchHolidays() async {
+    final apiKey = '6ejHzAidPCOiSeD1GyDUsAKSbob0Iwmx';
+    final year = DateTime.now().year;
+    final country = 'US';
+
+    final url = Uri.parse(
+        'https://calendarific.com/api/v2/holidays?&api_key=$apiKey&country=$country&year=$year');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        _holidays = data['response']['holidays'];
+      });
+      print(_holidays);
+    } else {
+      throw Exception('Failed to get holidays error');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Holidays'),
+      ),
+      body: ListView.builder(
+        itemCount: _holidays.length,
+        itemBuilder: (context, index) {
+          final holiday = _holidays[index];
+          return ListTile(
+            title: Text(holiday['name']),
+            subtitle: Text(holiday['date']['iso']),
+          );
+        },
+      ),
+    );
+  }
+}
